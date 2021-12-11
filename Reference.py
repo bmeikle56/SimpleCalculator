0282f31398f8e0601a1f85fd21be28129d72b178
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2021-12-10 19:38:23 -0500

    Add unit test and reference files

---
+++ Reference.py
@@ -0,0 +1,2 @@
+#Quick documentary for reference
+
---
+++ UnitTests.py
@@ -0,0 +1,3 @@
+#Unit Tests for this project
+#Followed Test Driven Development principles
+#These were created before or simultaneously with code development
